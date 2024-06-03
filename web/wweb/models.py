4b4b3de8b288da9377afddc138c83ca8037d1621
Samarth <153519599+Samarth24558@users.noreply.github.com>
2024-06-03 07:59:09 +0000

    Added fiels

--- web/wweb/models.py
+++ web/wweb/models.py
@@ -2,12 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Students(models.Model):
-    name=models.TextField()
-    mother_name=models.TextField()
-    father_name=models.TextField()
+    name=models.CharField(max_length=200)
+    mother_name=models.CharField(max_length=200)
+    father_name=models.CharField(max_length=200)
     address=models.CharField(max_length=200)
     gender=models.CharField(max_length=7)
     date=models.DateField()
-    course=models.TextField()
-    contact=models.TextField()
+    course=models.CharField(max_length=200)
+    contact=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
--- web/wweb/views.py
+++ web/wweb/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Students
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
 
 def manage(request):
+    if request.method=='POST':
+        name=request.POST.get('name')
+        mname=request.POST.get('mname')
+        fname=request.POST.get('fname')
+        address=request.POST.get('address')
+        gender=request.POST.get('gender')
+        dob=request.POST.get('dob')
+        course=request.POST.get('course')
+        contact=request.POST.get('contact')
+
+        data=Students(name=name,mother_name=mname,father_name=fname,address=address,gender=gender,date=dob,course=course,contact=contact)
+        data.save()
+        form=Students.objects.all()
+        context={'form':form}
+        return redirect('/view',context)
+        
     return render(request,'manage.html')
 
 def result(request):
@@ -15,4 +31,6 @@ def add_result(request):
     return render(request, 'add_result.html')
 
 def view(request):
-    return render(request,'view.html')
+    form=Students.objects.all()
+    context={'form':form}
+    return render(request,'view.html',context)
