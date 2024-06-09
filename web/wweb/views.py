68e0229e37a3b3e90d89ac9bc2882c00500f78ed
Samarth <153519599+Samarth24558@users.noreply.github.com>
2024-06-09 12:19:10 +0000

    Added files

--- web/wweb/views.py
+++ web/wweb/views.py
@@ -1,9 +1,44 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.models import User
 from django.contrib import messages
 from .models import *
 # Create your views here.
 
+def loginUser(request):
+    if request.method=="POST":
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        user=authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect("/")
+        messages.error(request,"Invalid username or password")
+    return render(request,"login.html")
+
+def logOut(request):
+    logout(request)
+    return redirect("/login")
+
+
+def signup(request):
+    if request.method=="POST":
+        name=request.POST.get("name")
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        rpassword=request.POST.get("rpassword")
+
+        if password!=rpassword:
+            messages.error(request,"Password not matched")
+        else:
+            User.objects.create_user(username=name,email=username,password=password)
+            return redirect("/login")
+    return render(request,"signup.html")
+
+
 def index(request):
+    if request.user.is_anonymous:
+        return redirect("/login")
     form=Result.objects.all()
     context={"form":form}
     return render(request,'index.html',context)
